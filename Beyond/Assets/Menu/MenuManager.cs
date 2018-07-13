8e5b2910c3ce6497eb969897d5d5e32316a74037
SnachGame <snachgame@gmail.com>
2018-07-13 21:29:52 +0700

    Меню в первом приближении

---
+++ Beyond/Assets/Menu/MenuManager.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Assets.Menu
+{
+    public class MenuManager : MonoBehaviour
+    {
+        private Resolution[] availableResolutions;
+        public Dropdown ResolutionDropdown;
+        public Dropdown QualityDropdown;
+        public Dropdown ScreenModeDropdown;
+        public AudioMixer AudioMixer;
+        public RectTransform MainMenuPanel;
+        public RectTransform OptionsPanel;
+
+        private void Awake()
+        {
+            InitResolutionDropdown();
+            InitQualityDropdown();
+            InitScreenModeDropdown();
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (OptionsPanel.gameObject.activeSelf)
+                {
+                    MainMenuPanel.gameObject.SetActive(true);
+                    OptionsPanel.gameObject.SetActive(false);
+                }
+            }
+        }
+
+        public void LoadLevel()
+        {
+            SceneManager.LoadSceneAsync("DummyGameScene", LoadSceneMode.Single);
+        }
+
+        public void Quit()
+        {
+#if UNITY_EDITOR
+            EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+
+        public void SetQuality(int qualityIndex)
+        {
+            QualitySettings.SetQualityLevel(qualityIndex);
+        }
+
+        public void SetResolution(int resolutionIndex)
+        {
+            Resolution resolution = availableResolutions[resolutionIndex];
+            Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+        }
+
+        public void SetScreenMode(int screenModeIndex)
+        {
+            Screen.fullScreenMode = (FullScreenMode)screenModeIndex;
+        }
+
+        public void SetCommonVolume(float volume)
+        {
+            AudioMixer.SetFloat("volume", volume);
+        }
+
+        private void InitScreenModeDropdown()
+        {
+            if (ScreenModeDropdown == null)
+                return;
+
+            var options = new List<string>();
+            foreach (FullScreenMode mode in (FullScreenMode[])Enum.GetValues(typeof(FullScreenMode)))
+                options.Add(mode.ToString());
+
+            ScreenModeDropdown.ClearOptions();
+
+            ScreenModeDropdown.AddOptions(options);
+            ScreenModeDropdown.value = (int)Screen.fullScreenMode;
+            ScreenModeDropdown.RefreshShownValue();
+        }
+
+        private void InitQualityDropdown()
+        {
+            if (QualityDropdown == null)
+                return;
+
+            QualityDropdown.ClearOptions();
+            QualityDropdown.AddOptions(QualitySettings.names.ToList());
+            QualityDropdown.value = QualitySettings.GetQualityLevel();
+            QualityDropdown.RefreshShownValue();
+        }
+
+        private void InitResolutionDropdown()
+        {
+            if (ResolutionDropdown == null)
+                return;
+
+            int currentResolutionIndex = 0;
+            var resolutionOptions = new List<string>();
+            availableResolutions = Screen.resolutions;        
+
+            for (int i = 0; i < availableResolutions.Length; i++)
+            {
+                resolutionOptions.Add(availableResolutions[i].ToString());
+
+                if (availableResolutions[i].Equals(Screen.currentResolution))
+                    currentResolutionIndex = i;
+            }
+
+            ResolutionDropdown.ClearOptions();
+            ResolutionDropdown.AddOptions(resolutionOptions);
+            ResolutionDropdown.value = currentResolutionIndex;
+            ResolutionDropdown.RefreshShownValue();
+        }
+    }
+
+    public static class ResolutionExtension
+    {
+        public static bool Equals(this Resolution thisResolution, Resolution resolution)
+        {
+            return thisResolution.width == resolution.width &&
+                thisResolution.height == resolution.height;
+        }
+    }
+}
---
+++ Beyond/Assets/Menu/MenuStarter.cs
@@ -0,0 +1,67 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace Assets.Menu
+{
+    public class MenuStarter : MonoBehaviour
+    {
+        private GameObject menu;
+        private Canvas canvas;
+        private RectTransform mainMenuPanel;
+        private RectTransform optionsPanel;
+        private RectTransform graphicsPanel;
+        private RectTransform soundPanel;
+        private RectTransform controlsPanel;
+
+        private void Start()
+        {
+            menu = Instantiate(Resources.Load("MenuUI")) as GameObject;
+            menu.SetActive(false);
+
+            InitUiElements();
+
+            canvas.sortingOrder = 1;
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (menu.activeSelf)
+                {
+                    menu.SetActive(false);
+                    Time.timeScale = 0;
+                }
+                else
+                {
+                    menu.SetActive(true);
+                    Time.timeScale = 1;
+
+                    SetDefaultActive();
+                }
+            }       
+        }
+
+        private void SetDefaultActive()
+        {
+            mainMenuPanel.gameObject.SetActive(true);
+            optionsPanel.gameObject.SetActive(false);
+            graphicsPanel.gameObject.SetActive(true);
+            soundPanel.gameObject.SetActive(false);
+            controlsPanel.gameObject.SetActive(false);
+        }
+
+        private void InitUiElements()
+        {
+            canvas = menu.GetComponent<Canvas>();
+
+            RectTransform[] rectTransform = canvas.GetComponentsInChildren<RectTransform>(true);
+
+            mainMenuPanel = rectTransform.FirstOrDefault(x => x.name == "MainMenuPanel");
+            optionsPanel = rectTransform.FirstOrDefault(x => x.name == "OptionsPanel");
+            graphicsPanel = rectTransform.FirstOrDefault(x => x.name == "GraphicsPanel");
+            soundPanel = rectTransform.FirstOrDefault(x => x.name == "SoundPanel");
+            controlsPanel = rectTransform.FirstOrDefault(x => x.name == "ControlsPanel");
+        }
+    }
+}
